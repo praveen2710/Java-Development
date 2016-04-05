dd8d7db18c6d626b1562817445339dff8b0f5df3
Praveen2710 <praveenbanthia27@gmail.com>
2016-04-04 22:13:25 -0500

    replaced integer with constant and made a minor correction to shape of triangle

--- UnfoldingMaps/UCSDUnfoldingMaps/src/module4/CityMarker.java
+++ UnfoldingMaps/UCSDUnfoldingMaps/src/module4/CityMarker.java
@@ -44,7 +44,7 @@ public class CityMarker extends SimplePointMarker {
 		
 		// TODO: Add code to draw a triangle to represent the CityMarker
 		pg.fill(255,20,30);
-		pg.triangle(x,y,x-10,y+10,x+10,y+10);
+		pg.triangle(x,y-TRI_SIZE,x-TRI_SIZE,y+TRI_SIZE,x+TRI_SIZE,y+TRI_SIZE);
 		// Restore previous drawing style
 		pg.popStyle();
 	}
