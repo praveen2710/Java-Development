d17d87fc53f34e116e52d71f7206114870803cb9
Praveen2710 <praveenbanthia27@gmail.com>
2016-05-15 09:43:49 -0500

    commiting to a point

--- ProofOfConcepts/Gradle/Spring-MVC/src/main/java/com/beabettercoder/springconfig/WebConfiguration.java
+++ ProofOfConcepts/Gradle/Spring-MVC/src/main/java/com/beabettercoder/springconfig/WebConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
- 
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.beabettercoder.controller")
