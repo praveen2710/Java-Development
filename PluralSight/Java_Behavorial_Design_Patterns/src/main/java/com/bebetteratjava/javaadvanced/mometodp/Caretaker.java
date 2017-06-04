36fd72d1c7eebbe24d12b0e8e78d3b2c997b6c08
Praveen Banthia <pbanthia@masonlive.gmu.edu>
2017-06-04 15:53:50 -0500

    example of momento dp

---
+++ PluralSight/Java_Behavorial_Design_Patterns/src/main/java/com/bebetteratjava/javaadvanced/mometodp/Caretaker.java
@@ -0,0 +1,19 @@
+package com.bebetteratjava.javaadvanced.mometodp;
+
+import java.util.Stack;
+
+//caretaker
+public class Caretaker {
+
+
+	//some people prefer arraylist however using stack has advantage of saving sequence;
+	private Stack<EmployeeMemento> employeeHistory = new Stack<>();
+	
+	public void save(Employee emp) {
+		employeeHistory.push(emp.save());
+	}
+	
+	public void revert(Employee emp) {
+		emp.revert(employeeHistory.pop());
+	}
+}
